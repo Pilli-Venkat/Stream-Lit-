f7fcea7344521f330dcfc4b202fd0e95561f3d00
Pilli-Venkat <venkatasai2920@gmail.com>
2023-04-19 18:57:38 +0530

    Basic Changes

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import streamlit as st
 
 def find_length(a, b, c):
   return sorted([a,b,c],reverse= True)[0]
-# Set the title of the application
-st.title('Find Length Among Three Numbers')
+
+
+st.title('Largest Among Three Numbers - 22DS1000243 (PILLI SAI)')
 
 # Get the user input for three numbers
+
 num1 = st.number_input('Enter the first number')
 num2 = st.number_input('Enter the second number')
 num3 = st.number_input('Enter the third number')
 
-# Get the length among the three given numbers
-if st.button('Find Length'):
+
+if st.button('Largest Number'):
     length = find_length(num1, num2, num3)
-    st.success('The length among the three given numbers is: ' + str(length))
+    st.success('Largest Among three numbers is '+[num1,num2,num3]+" " + str(length))
